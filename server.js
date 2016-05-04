3b5e76c0d226fdf78a5f23b5e65b18ffe949bc97
Tessa Serdar <tessajeanserdar@gmail.com>
2016-05-04 15:33:05 -0700

    allowing heroku to dynamically set port

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 var express = require('express');
 var app = express();
 app.use(express.static('client'));
-
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
-});
+app.listen(process.env.PORT || 5000)
