88f2eaf6ae7629442d271cb5b7e0bcb680eaa43c
Tessa Serdar <tessajeanserdar@gmail.com>
2016-05-05 10:44:51 -0700

    set backgrond to blue gradient

---
+++ client/app.js
@@ -0,0 +1,32 @@
+// angular.module('tessaserdar')
+
+
+// .config(function($stateProvider, $urlRouterProvider) {
+
+//   $urlRouterProvider.otherwise("/images");
+//   $stateProvider
+//     .state('loadImages', {
+//       url: "/images",
+//       templateUrl: "/images/images.html",
+//     })
+//   $stateProvider
+//       .state('least', {
+//           url: "/least",
+//           templateUrl: "/leastliked/least.html",
+//         })
+//   $stateProvider
+//       .state('dash', {
+//           url: "/dash",
+//           templateUrl: "/photometa/photometa.html",
+//       })
+//   $stateProvider
+//     .state('signout', {
+//         url: "/signout",
+//         templateUrl: "/logout.html",
+//       })
+// })
+// .controller("main",function($scope,$location){
+//   $scope.logOut = (function(){
+//      $location.path('/signout');
+//   })
+// })
